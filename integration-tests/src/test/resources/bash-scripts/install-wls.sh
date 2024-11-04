4b9f56802a1fd5aed59904e1ce9c38c9fa310870
Sankar Neelakandan <sankar.periyathambi.neelakand@oracle.com>
2024-11-04 14:28:14 -0800

    Add WLS install script

---
+++ integration-tests/src/test/resources/bash-scripts/install-wls.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Copyright (c) 2024, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+env
+#wget http://home.us.oracle.com/results/release/src141200/fmw_14.1.2.0.0_wls_generic.jar
+#java -jar fmw_14.1.2.0.0_wls_generic.jar -silent -responseFile /scratch/opc/depot/localinstall/silent.response -invPtrLoc /scratch/opc/depot/localinstall/oraInst.loc
