89bc4f215d7cb4343cd347e4cbfcdf5b162dfff3
Sankar Neelakandan <sankar.periyathambi.neelakand@oracle.com>
2024-11-05 09:42:39 -0800

    wip

--- integration-tests/src/test/resources/bash-scripts/install-wls.sh
+++ integration-tests/src/test/resources/bash-scripts/install-wls.sh
@@ -2,17 +2,15 @@
 # Copyright (c) 2024, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-id
-whoami
-env
 echo $JAVA_HOME
 echo $result_root
 MW_HOME="$result_root/mwhome"
 SILENT_RESPONSE_FILE=$result_root/silent.response
 ORAINVENTORYPOINTER_LOC=$result_root/oraInv.loc
 ORAINVENTORY_LOC=$result_root/oraInventory
-WLS_SHIPHOME=$result_root/fmw_12.2.1.4.0_wls_generic.jar
-DOWNLOAD_URL="http://$SHIPHOME_DOWNLOAD_SERVER/results/release/src122140psu/fmw_12.2.1.4.0_wls_generic.jar"
+WLS_SHIPHOME=$result_root/fmw_wls_generic.jar
+DOWNLOAD_URL="http://$SHIPHOME_DOWNLOAD_SERVER/results/release/src141200/fmw_14.1.2.0.0_wls_generic.jar"
+SUCCESS="The\ installation\ of\ Oracle\ Fusion\ Middleware.*completed\ successfully"
 
 echo "creating $SILENT_RESPONSE_FILE file with contents"
 
@@ -36,9 +34,6 @@ EOF
 cat $ORAINVENTORYPOINTER_LOC
 
 #download WebLogic shiphome installer
-if [[ "$WEBLOGIC_IMAGE_TAG" == *"14.1.2"* ]]; then
-  DOWNLOAD_URL="http://$SHIPHOME_DOWNLOAD_SERVER/results/release/src141200/fmw_14.1.2.0.0_wls_generic.jar"
-fi
 curl -Lo $WLS_SHIPHOME $DOWNLOAD_URL
 ls -l $WLS_SHIPHOME
 md5sum $WLS_SHIPHOME
@@ -48,8 +43,9 @@ mkdir -p $MW_HOME
 mkdir -p $ORAINVENTORY_LOC
 
 echo "Running java -jar $WLS_SHIPHOME -silent -responseFile $SILENT_RESPONSE_FILE -invPtrLoc $ORAINVENTORYPOINTER_LOC"
-java -jar $WLS_SHIPHOME -silent -responseFile $SILENT_RESPONSE_FILE -invPtrLoc $ORAINVENTORYPOINTER_LOC
-echo $?
-
-ls -l $MW_HOME
-cat /tmp/OraInstall2024*
+install_log=$(java -jar $WLS_SHIPHOME -silent -responseFile $SILENT_RESPONSE_FILE -invPtrLoc $ORAINVENTORYPOINTER_LOC)
+if [[ "$install_log" =~ $SUCCESS ]]; then
+  echo "The installation of WebLogic completed successfully."
+else
+  echo "The installation of WebLogic failed."
+fi
