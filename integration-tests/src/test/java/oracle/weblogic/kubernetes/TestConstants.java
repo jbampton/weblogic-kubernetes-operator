340069bf726d94693841860a383be974e46b69fb
Sankar Neelakandan <sankar.periyathambi.neelakand@oracle.com>
2025-02-13 09:06:23 -0800

    wip

--- integration-tests/src/test/java/oracle/weblogic/kubernetes/TestConstants.java
+++ integration-tests/src/test/java/oracle/weblogic/kubernetes/TestConstants.java
@@ -107,9 +107,9 @@ public interface TestConstants {
   // It depends on the default value of BASE_IMAGES_REPO. 
   // Following defaults are assumining OCIR as default for BASE_IMAGES_REPO.
   public static final String WEBLOGIC_IMAGE_NAME_DEFAULT = "test-images/weblogic";
-  public static final String WEBLOGIC_IMAGE_TAG_DEFAULT = "12.2.1.4";
+  public static final String WEBLOGIC_IMAGE_TAG_DEFAULT = "14.1.2.0-generic-jdk17-ol8";
   public static final String FMWINFRA_IMAGE_NAME_DEFAULT = "test-images/fmw-infrastructure";
-  public static final String FMWINFRA_IMAGE_TAG_DEFAULT = "12.2.1.4";
+  public static final String FMWINFRA_IMAGE_TAG_DEFAULT = "14.1.2.0-jdk17-ol8";
   public static final String FMWINFRA_IMAGE_TAG_12213 = "12.2.1.3";
   public static final String DB_IMAGE_NAME_DEFAULT = "test-images/database/enterprise";
   public static final String DB_PREBUILT_IMAGE_NAME_DEFAULT = "test-images/database/express";
